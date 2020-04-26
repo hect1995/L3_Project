3129533b0240455c249799bac1047e820b2550a0
Hector <hect.esteban@gmail.com>
2020-04-26 17:37:49 +0200

    mend

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <future>
 #include <random>
+#include <mutex>
 
 #include "Street.h"
 #include "Intersection.h"
@@ -15,11 +16,13 @@
 
 int WaitingVehicles::getSize()
 {
+    std::unique_lock<std::mutex> mtx;
     return _vehicles.size();
 }
 
 void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise)
 {
+    std::unique_lock<std::mutex> mtx1;
     _vehicles.push_back(vehicle);
     _promises.push_back(std::move(promise));
 }
@@ -27,6 +30,7 @@ void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<vo
 void WaitingVehicles::permitEntryToFirstInQueue()
 {
     // get entries from the front of both queues
+    std::unique_lock<std::mutex> mtx2;
     auto firstPromise = _promises.begin();
     auto firstVehicle = _vehicles.begin();
 
@@ -70,14 +74,16 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
     // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held. 
-
+    std::unique_lock<std::mutex> _mtxCout;
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
+    _mtxCout.unlock();
 
     // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
     std::future<void> ftrVehicleAllowedToEnter = prmsVehicleAllowedToEnter.get_future();
     _waitingVehicles.pushBack(vehicle, std::move(prmsVehicleAllowedToEnter));
-
+    _mtxCout.lock();
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
