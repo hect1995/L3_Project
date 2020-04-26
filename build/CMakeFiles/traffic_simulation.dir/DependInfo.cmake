11f0e41db1e5686333e1b23a92e41782fd84e57c
Hector <hect.esteban@gmail.com>
2020-04-26 17:37:10 +0200

    second

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/Graphics.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/Intersection.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/Street.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/TrafficObject.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/TrafficSimulator-L3.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L3.cpp.o"
+  "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/src/Vehicle.cpp" "/Users/hectoresteban/Documents/C++/UdacityC++/L3_Project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.2.0_3/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
